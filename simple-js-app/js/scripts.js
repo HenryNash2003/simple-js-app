f1d55a5170930213cacdc015bdedca576d4af9c5
HenryNash2003 <v.oates@outlook.com>
2022-06-28 14:25:43 -0400

    Created PokeDex
    Listing of Pokemon creatures, with name, height, type,

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,11 @@
+let pokemon list = [{name:'Bulbasaur', height:7.0, type:['grass', ' poison']},
+      {name:'Fearow', height: 1.2, type:['flying',' normal']},
+     {name:'Butterfree', height:1.1, type:['bug', ' flying']},
+     {name:'Nidoking', height:5.03, type:['ground', 'poison']},
+     {name:'Golbat', height:1.5, type:['flying', ' poison']},
+     {name:'Arcanine', height:8.0, type:['fire']},
+     {name:'Persian', height:3.7, type:['ground', 'normal']},
+     {name:'Raichu', height:2.00, type:['electric']},
+     {name:'Machamp', height:9.3, type:['ground', 'fighting']},
+     {name:'Ninetails', height: 12.5, type:['ground', 'fire']}
+     ]
